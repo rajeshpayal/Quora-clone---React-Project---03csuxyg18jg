633b7e9a67d889ff2cb18ed0dd656095aaea0449
Rajesh Kumar <92311520+rajeshpayal@users.noreply.github.com>
2023-03-01 12:24:36 +0000

    first

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,11 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyArGeHaWkZ3wIvfXJUROUKdxZfEw73j5HM",
-  authDomain: "quora-clone-1b761.firebaseapp.com",
-  databaseURL: "https://quora-clone-1b761-default-rtdb.firebaseio.com",
-  projectId: "quora-clone-1b761",
-  storageBucket: "quora-clone-1b761.appspot.com",
-  messagingSenderId: "5474927088",
-  appId: "1:5474927088:web:7dff5a7f8b5e9b5d575d35",
+  apiKey: "AIzaSyDycChWNDoqMtKHHNwxu8OZ8yr5jid-Qnk",
+  authDomain: "quroaclone.firebaseapp.com",
+  projectId: "quroaclone",
+  storageBucket: "quroaclone.appspot.com",
+  messagingSenderId: "727515719094",
+  appId: "1:727515719094:web:c874d7bcaee0aa316d4d17",
   measurementId: "G-RS6THMYKEZ",
 };
 
